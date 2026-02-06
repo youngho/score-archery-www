a14e5fbe1611b6f031c94bf333828cd554acc1bb
youngho <xfile0304@gmail.com>
2026-02-06 10:39:30 +0900

    현재 https 안됨 http로 변경

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // 프로덕션 환경 설정
 export const environment = {
   production: true,
-  apiUrl: 'https://158.179.161.203:8080',
+  apiUrl: 'http://158.179.161.203:8080',
   appName: 'The Score of Archery',
   appVersion: '1.0.0',
   supportEmail: 'support@arrowmaster.com',
